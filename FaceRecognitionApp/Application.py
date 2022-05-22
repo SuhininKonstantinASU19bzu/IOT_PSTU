b87f226943ec5f745a28d926458efb94f8299b7d
CharnyyAleksandrASU19bzu <65458983+CharnyyAleksandrASU19bzu@users.noreply.github.com>
2022-05-22 16:15:59 +0500

    add_knownpeople, add_uploadphoto
    Добавлены функции в Apllication

--- FaceRecognitionApp/Application.py
+++ FaceRecognitionApp/Application.py
@@ -3,6 +3,26 @@ from fastapi import FastAPI, File, UploadFile
 #from FaceIdentification import 
 
 app = FastAPI()
+myFI = SimpleFaceRecognizer()
+
+@app.get("/knownpeople/")
+async def getKnownPeople():
+myFI.load_new_images();
+return {"knownPeople": f{"myFI.known_faces()}"}
+
+@app.post("/uploadphoto/")
+async def uploadPhoto(name: str, photo: UploadFile=File(...)):
+    contents=await photo.read()
+    try:
+        if myFI.add_person(name, contents):
+            result = f"person {name} with photo {photo.filename} uploaded"
+        else:
+            result = "not one person"
+    except:
+        result = "Error!"
+return {"upload_result": result}
+
+        
 
 
 if __name__ == "__Application__":
